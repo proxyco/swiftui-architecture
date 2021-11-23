ba083a497505f76e803a6895a81bd8dbcbdf0a61
Facundo Menzella <facumenzella@gmail.com>
2021-11-23 11:14:47 +0100

    SwiftUI double tap for devmenu

--- ProxyRelease/ProxyReleaseApp.swift
+++ ProxyRelease/ProxyReleaseApp.swift
@@ -7,11 +7,30 @@
 
 import SwiftUI
 
+final class AppViewModel: ObservableObject {
+    @Published var devMenu = false
+}
+
 @main
 struct ProxyReleaseApp: App {
+    @ObservedObject var viewModel = AppViewModel()
+
     var body: some Scene {
         WindowGroup {
             RootView(viewModel: RootViewModel())
+                .simultaneousGesture(
+                    TapGesture(count: 2)
+                        .onEnded({ _ in
+                            // check certificate and build configuration
+                            viewModel.devMenu = true
+                        })
+                )
+                .sheet(
+                    isPresented: $viewModel.devMenu,
+                    content: {
+                        Text("DevMenu")
+                    }
+                )
         }
     }
 }
