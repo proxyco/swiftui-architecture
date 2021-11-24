ade48ce191b5972ee294c698696ee36c26e69ec6
Andres Canal <andresyo990@gmail.com>
2021-11-24 10:32:44 -0300

    some progress

--- ProxyRelease/NavigationSampleView/NavigationSampleViewModel.swift
+++ ProxyRelease/NavigationSampleView/NavigationSampleViewModel.swift
@@ -7,21 +7,18 @@
 
 import Foundation
 import SwiftUINavigation
+import SwiftUI
 
 enum Route {
-    case openViewA
-    case openViewB
+    case editUser(_ user: User?)
 }
 
 final class NavigationSampleViewModel: ObservableObject {
 
     @Published var route: Route?
+    @Published var user: User = User(firstName: "", lastName: "")
 
-    func openViewA() {
-        route = .openViewA
-    }
-
-    func openViewB() {
-        route = .openViewB
+    func editUser() {
+        route = .editUser(user)
     }
 }
